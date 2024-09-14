3fe39be51e339838707d02ed4ec4d971c055c324
Delina <delinayemane2004@gmail.com>
2024-09-14 14:11:55 -0700

    Add project files.

---
+++ HW6/HW6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string street;
+    cout << "Street: " ;
+    cin >> street;
+
+    string city, state;  int ZP;
+    cout << "City, State, Zipcode";
+    cin >> city, state, ZP;
+
+    
+    return 0;
+}
